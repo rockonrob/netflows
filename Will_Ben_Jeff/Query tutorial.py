dc7406e98c5be553b5253e814a9ce185dac58994
rockonrob <williamk03hler@gmail.com>
2016-04-03 15:54:40 -0500

    seperated cat and cont var to to DBSCAN

--- Will_Ben_Jeff/Query tutorial.py
+++ Will_Ben_Jeff/Query tutorial.py
@@ -29,24 +29,25 @@ cnx.close()
 
 #fil = os.path.dirname('C:/Users/William Koehler/Documents/Stats 427/New folder/netflows_all.csv')
 start = default_timer()
-df = pd.read_csv('C:/Users/William Koehler/Documents/Stats 427/New folder/netflows_50k.csv', header=0)
+df = pd.read_csv('netflows_50k.csv', header=0)
 print('It takes about: {0:2f} seconds to import the entire datasetn\n'.format(default_timer() - start))
 print('Head of the dataframe:\n')
 print(df.head(n=15))
 
+cont_var = ['src_ip','dst_ip', 'packets','octets','start_time','start_msec','end_time','end_msec','src_port','dst_port']
+cat_var = ['tcp_flags','prot']
+
 print('Starting DBSCAN to find outliers on the points')
 start = default_timer()
 dbs = DBSCAN(eps=10000, min_samples=2)
-dbs.fit(df)
-print('It takes {0:2f} seconds to fully complete the DBSCAN with eps=.25 and minSamples=10\n It also takes about 4~8gb of memory'.format(default_timer() - start))
+dbs.fit(df[cont_var])
+print('It takes {0:2f} seconds to fully complete the DBSCAN with eps=10000 and minSamples=2\n It also takes about 4~8gb of memory for 14mil'.format(default_timer() - start))
 
 from collections import Counter
 
 print('DBSCAN Cluster membership:\n', 25*'-')
 for key, val in Counter(dbs.labels_).most_common():
     print('Class {0:2d} has {1:3d} members'.format(key, val))
-    
-    
     
 
 #of = 0.1 # Expected Outlier Fraction
